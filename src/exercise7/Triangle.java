f25fe34ad8bc70f8398de66df63646b005c51fc7
mihaelastoychev <>
2021-12-22 17:56:42 +0000

    reshenie

--- src/exercise7/Triangle.java
+++ src/exercise7/Triangle.java
@@ -3,25 +3,27 @@ package exercise7;
 import java.util.Scanner;
 
 public class Triangle {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter n: ");
-        int n = input.nextInt();
-
-        for (int line = 1; line <= n; line++) {
-            printLine(1, line);
+    public static void printTriangle (int size){
+        for (int i = 1; i <= size; i++){
+            printNumbers(i);
         }
 
-        for (int line = n - 1; line >= 1; line--) {
-            printLine(1, line);
+        for (int i = size - 1; i >= 1; i--){
+            printNumbers(i);
         }
     }
 
-    public static void printLine(int start, int end) {
-        for (int i = start; i <= end; i++) {
-            System.out.print(" " + i);
+    public static void printNumbers (int size){
+        for (int i = 1; i <= size; i++){
+            System.out.print(i + " ");
         }
         System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+
+        printTriangle(size);
     }
 }
